bc5d5ed59b0c972f010864e0916fbbeaf7a407d1
AndreiRupertti <arupertti@gmail.com>
2017-09-21 17:15:28 -0300

    Melhora o codigo

--- romanosRefeito.js
+++ romanosRefeito.js
@@ -1,16 +1,6 @@
-function traduz(element, index, array) {
-  let num=romanos[element];
-  if(anterior!=0 && anterior<num){
-    decimal-=anterior;
-    num=num-anterior;
-    decimal+=num;
-  }else{
-    decimal+=num;
-    anterior=num;
-  }
-}
-/////////////////////////////////////////////////////////////////////
-romanos={
+let resultado=0, anterior=0;// VARIAVEIS DECIMAL(RESULTADO) E ANTERIOR(ULTIMO NUMERO SOMADO)
+
+romanos={//CRIANDO OBJ COM OS NUMEROS ROMANOS
   'I':1,
   'V':5,
   'X':10,
@@ -18,12 +8,27 @@ romanos={
   'C':100,
   'M':1000
 }
-let decimal=0, anterior=0;
-numeroRomano='CXXV'
-numeroRomano=numeroRomano.split('');
-numeroRomano.forEach(traduz);
-console.log(decimal);
-// logs:
-// a[0] = 2
-// a[1] = 5
-// a[3] = 9
+
+numeroRomano='CXXV'//SETANDO NUMEOR A SER TRADUZIDO
+numeroRomano=numeroRomano.split('');//SEPARANDO AS LETRAS NA STRING E COLOCANDO EM UM ARRAY
+numeroRomano.forEach(traduz);//CHAMANDO A FUNÇÃO TRADUZ PARA CADA ITEN DO ARRAY
+
+console.log(resultado);//IMPRIMI RESULTADO
+
+
+function traduz(element, index, array) {// FUNÇÃO TRADUZ QUE PEGA UM ELEMENTO DO ARRAY
+  let num=romanos[element];//NUM RECEBENDO O NUMERO CORRESPONDENDO DO ELEMENTO DO ARRAY(EX: se for "I" traz 1)
+
+  if(anterior!=0 && anterior<num){//SE O ANTERIOR N TIVER ZERADO E FOR MENOR DOQ O NUMERO DO ELEMENTO
+
+    resultado-=anterior;//RESULTADO RETIRA A ULTIMA SOMA FEITA
+    num=num-anterior;// NUM RECEBE RESULTADO DA SUBTRAÇAO DO ULTIMO NUMERO COM O NOVO
+    resultado+=num;//E ACRESCENTA NO RESULTADO
+
+  }else{ // SOMA NORMAL
+
+    resultado+=num;//RESULTADO ACRESCENTA O NOVO NUMERO
+    anterior=num;//ANTERIOR RECEBE O NUMERO Q FOI ACRESCENTADO
+
+  }
+}
